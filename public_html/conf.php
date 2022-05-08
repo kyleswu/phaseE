125aaeecb396a58f12acf144560dc39bab11b9e0
mihirc2000 <91142188+mihirc2000@users.noreply.github.com>
2022-05-08 14:48:27 -0400

    Add files via upload

---
+++ public_html/conf.php
@@ -0,0 +1,9 @@
+
+<?php
+	//these are credentials which will be used by open.php to create connection to dbase
+	//make updates here as noted below, and save this file as conf.php
+	$dbhost = "dbase.cs.jhu.edu"; // database host
+	$dbuser = "22sp_mchakra9";    // username on db host: replace 22sp_jhopkin1 with yours
+	$dbpass = "Fx8Or5Q9Fw";            // password on db host: replace ***** with yours
+	$dbname = "22sp_mchakra9_db"; // the db's name: replace 22sp_jhopkin1_db with yours
+?>
