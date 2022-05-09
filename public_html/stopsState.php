2cab73c7bbd87b1acf538fa5541c56e589c26a7f
kyleswu <kwu45@jhu.edu>
2022-05-09 17:30:16 -0400

    working

--- public_html/stopsState.php
+++ public_html/stopsState.php
@@ -1,4 +1,4 @@
-<head><title>Partisanship Data (State)</title></head>
+<head><title>Aggregate and Individual Stops Data for a Given State</title></head>
 <body>
 <?php
 
@@ -7,33 +7,28 @@
     $state = $_POST['state'];
 
     if (!empty($state)) {
-        echo "<h2>Partisanship Data for ";
+        echo "<h2>Aggregate and Individual Stops Data for ";
         echo $state;
         echo "</h2><br>";
-        if ($stmt = $conn->prepare("SELECT year, party
-                                    FROM Partisanship
-                                    WHERE state = ?;")) {
+
+        if ($stmt = $conn->prepare("SELECT COUNT(stopID) AS numStops
+                                   FROM Stop
+                                   WHERE state = ?;")) {
             
             $stmt->bind_param("s", $state);
 
             if ($stmt->execute()) {
-                
+
                 $result = $stmt->get_result();
 
                 if (($result) && ($result->num_rows != 0)) {
-                    
-                    echo "<table border=\"1px solid black\">";
-                    echo "<tr><th> Year </th> <th> Party </th></tr>";
-
-                    while ($row = $result->fetch_row()) {
-                        echo "<tr>";
-                        echo "<td>".$row[0]."</td>";
-                        echo "<td>".$row[1]."</td>";
-                        echo "</tr>";
+                    if ($row = $result->fetch_row()) {
+                        echo "Total stops: ";
+                        echo $row[0];
                     }
 
                 } else {
-                    echo "<div style='color:red;'>No partisanship data for this state<div><br>";
+                    echo "<div style='color:red;'>No Stops data for this state<div><br>";
                 }
 
                 $result->free_result();
@@ -49,8 +44,6 @@
             $error = $conn->errno . ' ' . $conn->error;
             echo $error; 
         }
-
-
 
     } else {
         echo "<div style='color: red;'>Please enter a state.</div><br>";
